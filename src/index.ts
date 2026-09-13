02d1c2eb8ba480354a179fab554ff7c0f246c274
phamvanquyit <phamvanquy.it@gmail.com>
2026-09-13 09:20:17 +0700

    feat: export pagination extras and desktop header props

--- src/index.ts
+++ src/index.ts
@@ -1,231 +1,199 @@
-export { App, useApp, useAppConfig, usePopupContainer, useToken } from "./app/App";
 export type { AppProps, NonlaAppConfig } from "./app/App";
-
+export { App, useApp, useAppConfig, usePopupContainer, useToken } from "./app/App";
+export type { ButtonColor, ButtonGroupProps, ButtonProps, ButtonSize, ButtonType, ButtonVariant } from "./button/Button";
 export { Button } from "./button/Button";
-export type { ButtonProps, ButtonGroupProps, ButtonType, ButtonSize, ButtonColor, ButtonVariant } from "./button/Button";
-export { ButtonCopy } from "./button/ButtonCopy";
 export type { ButtonCopyProps } from "./button/ButtonCopy";
-
-export { Input, TextArea, InputNumber } from "./input/Input";
-export type { InputProps, TextAreaProps, InputNumberProps, InputSize, TextAreaRef, PasswordProps } from "./input/Input";
-export { SearchInput } from "./input/SearchInput";
+export { ButtonCopy } from "./button/ButtonCopy";
+export type { InputNumberProps, InputProps, InputSize, PasswordProps, TextAreaProps, TextAreaRef } from "./input/Input";
+export { Input, InputNumber, TextArea } from "./input/Input";
 export type { SearchInputProps } from "./input/SearchInput";
+export { SearchInput } from "./input/SearchInput";
 /** Native input element ref. */
 export type InputRef = HTMLInputElement;
 
-export { Select, SelectOption } from "./select/Select";
-export type { SelectProps, SelectOptionConfig, SelectValue } from "./select/Select";
-
-export { Switch } from "./switch/Switch";
-export type { SwitchProps, SwitchVariant } from "./switch/Switch";
-
-export { Checkbox } from "./checkbox/Checkbox";
-export type { CheckboxProps } from "./checkbox/Checkbox";
-
-export { ColorPicker, Color } from "./colorpicker/ColorPicker";
-export type { ColorPickerProps, ColorFormat, ColorType, PresetColorType, ColorPickerSemanticSlot } from "./colorpicker/ColorPicker";
-
-export { Tooltip } from "./tooltip/Tooltip";
-export type { TooltipProps, TooltipPlacement } from "./tooltip/Tooltip";
-
-export { Modal } from "./modal/Modal";
-export type { ModalProps, ModalConfirmProps } from "./modal/Modal";
-
-export { message } from "./message/message";
-export type { MessageType, MessageConfig } from "./message/message";
-
-export {
-  SchemaForm,
-  FormSchema,
-  FormItem,
-  EFormItemType,
-  hydrateRules,
-  fieldNameOf,
-  isRuleRequired,
-} from "./form";
+export type { AlertProps, AlertType } from "./alert/Alert";
+export { Alert } from "./alert/Alert";
+export type { CalendarProps } from "./calendar/Calendar";
+export { Calendar } from "./calendar/Calendar";
 export type {
-  SchemaFormProps,
-  FormItemProps,
+  AgentHistoryMessage,
+  AgentMessage,
+  AgentMessageRole,
+  AgentPanelEndpoint,
+  AgentPanelProps,
+  AgentStreamRequest,
+  AgentToolAction,
+  AgentToolCallEvent,
+  AgentToolHook,
+  AgentToolNameMatch,
+  AgentToolResultEvent,
+  AgentToolUI,
+  AgentToolUIName,
+  BackgroundTasksBarProps,
+  ChatAgentMessageProps,
+  ChatBgTask,
+  ChatErrorProps,
+  ChatInputProps,
+  ChatMarkdownProps,
+  ChatMarkdownStreamState,
+  ChatThinkingProps,
+  ChatToolCallProps,
+  ChatToolMessage,
+  ChatUserMessageProps,
+  ChatWelcomeProps,
+  MermaidBlockProps,
+  ToolUIProps,
+} from "./chat";
+export {
+  AgentPanel,
+  BackgroundTasksBar,
+  BackgroundTaskToolUI,
+  buildAgentHistory,
+  builtinToolUis,
+  CallAgentToolUI,
+  ChatAgentMessage,
+  ChatError,
+  ChatInput,
+  ChatMarkdown,
+  ChatSpinner,
+  ChatThinking,
+  ChatToolCall,
+  ChatUserMessage,
+  ChatWelcome,
+  chatBodyClass,
+  chatMarkdownClass,
+  chatMarkdownComponents,
+  createChatMarkdownComponents,
+  formatBgElapsed,
+  formatToolName,
+  GetCurrentTimeToolUI,
+  isCallAgentToolName,
+  isToolRunning,
+  MermaidBlock,
+  matchesToolHook,
+  matchesToolName,
+  matchesToolUIName,
+  parseBgTaskRef,
+  parseCallAgentToolTargetId,
+  parseSseStream,
+  prettyJson,
+  ReadSkillToolUI,
+  RunJsToolUI,
+  resolveToolUI,
+  ToolUiTrailing,
+  useAgentStream,
+  WebFetchToolUI,
+} from "./chat";
+export type { CheckboxProps } from "./checkbox/Checkbox";
+export { Checkbox } from "./checkbox/Checkbox";
+export type { CodeBlockCopyButtonProps, CodeBlockProps } from "./codeblock/CodeBlock";
+export { CodeBlock, CodeBlockCopyButton } from "./codeblock/CodeBlock";
+export type { ColorFormat, ColorPickerProps, ColorPickerSemanticSlot, ColorType, PresetColorType } from "./colorpicker/ColorPicker";
+export { Color, ColorPicker } from "./colorpicker/ColorPicker";
+export type { DatePickerProps, RangePickerProps, RangeValue } from "./datepicker/DatePicker";
+export { DatePicker, RangePicker } from "./datepicker/DatePicker";
+export type { DesktopHeaderProps } from "./desktop/DesktopHeader";
+export { DesktopBarButton, DesktopBarDivider, DesktopBarItem, DesktopHeader } from "./desktop/DesktopHeader";
+export { DesktopIcon } from "./desktop/DesktopIcon";
+export { DesktopStage } from "./desktop/DesktopStage";
+export type { DesktopWindowProps, WindowHeaderProps } from "./desktop/DesktopWindow";
+export { DesktopWindow, WindowHeader } from "./desktop/DesktopWindow";
+export { MeadowDesktop } from "./desktop/MeadowDesktop";
+export { MeadowShell } from "./desktop/MeadowShell";
+export { MeadowWallpaper } from "./desktop/MeadowWallpaper";
+export type { DrawerPlacement, DrawerProps } from "./drawer/Drawer";
+export { Drawer } from "./drawer/Drawer";
+export type { ContextMenuProps } from "./dropdown/ContextMenu";
+
+export { ContextMenu } from "./dropdown/ContextMenu";
+export type { DropdownProps, MenuItemType, MenuProps } from "./dropdown/Dropdown";
+export { Dropdown } from "./dropdown/Dropdown";
+export type { EmptyProps } from "./empty/Empty";
+export { Empty } from "./empty/Empty";
+export type {
   FormFetcher,
+  FormItemProps,
+  IFormItemHelpProps,
+  ISelectItemProps,
+  SchemaFormProps,
   TForm,
   TFormItemProps,
   TFormRule,
   TRuleValueMessage,
-  ISelectItemProps,
-  IFormItemHelpProps,
 } from "./form";
-
+export {
+  EFormItemType,
+  FormItem,
+  FormSchema,
+  fieldNameOf,
+  hydrateRules,
+  isRuleRequired,
+  SchemaForm,
+} from "./form";
+export type { FormLayoutItemProps, FormLayoutProps } from "./form-layout/FormLayout";
 /** Layout-only Form + Form.Item for labeled fields. */
 export { Form } from "./form-layout/FormLayout";
-export type { FormLayoutProps, FormLayoutItemProps } from "./form-layout/FormLayout";
-
-export { Alert } from "./alert/Alert";
-export type { AlertProps, AlertType } from "./alert/Alert";
-
-export { Popover } from "./popover/Popover";
-export type { PopoverProps } from "./popover/Popover";
-
-export { Dropdown } from "./dropdown/Dropdown";
-export type { DropdownProps, MenuItemType, MenuProps } from "./dropdown/Dropdown";
-
-export { ContextMenu } from "./dropdown/ContextMenu";
-export type { ContextMenuProps } from "./dropdown/ContextMenu";
-
-export { Popconfirm } from "./popconfirm/Popconfirm";
-export type { PopconfirmProps } from "./popconfirm/Popconfirm";
-
-export { Drawer } from "./drawer/Drawer";
-export type { DrawerProps, DrawerPlacement } from "./drawer/Drawer";
-
-export { Splitter, SplitterPanel } from "./splitter/Splitter";
-export type { SplitterProps, SplitterPanelProps, SplitterOrientation, SplitterSize, SplitterSemanticSlot } from "./splitter/Splitter";
-
-export { Table } from "./table/Table";
-export type {
-  TableProps,
-  ColumnType,
-  ColumnsType,
-  TablePaginationConfig,
-  TableRowSelection,
-  SortOrder,
-} from "./table/Table";
-
-export { Tag } from "./tag/Tag";
-export type { TagProps, TagVariant } from "./tag/Tag";
-
-export { Pagination } from "./pagination/Pagination";
-export type { PaginationProps, PaginationItemType } from "./pagination/Pagination";
-
-export { Empty } from "./empty/Empty";
-export type { EmptyProps } from "./empty/Empty";
-
-export { Spin } from "./spin/Spin";
-export type { SpinProps, SpinVariant } from "./spin/Spin";
-
-export { Skeleton } from "./skeleton/Skeleton";
-export type { SkeletonProps } from "./skeleton/Skeleton";
-
-export { Shimmer } from "./shimmer/Shimmer";
-export type { ShimmerProps } from "./shimmer/Shimmer";
-
-export { Segmented } from "./segmented/Segmented";
-export type { SegmentedProps, SegmentedOption } from "./segmented/Segmented";
-
-export { DatePicker, RangePicker } from "./datepicker/DatePicker";
-export type { DatePickerProps, RangePickerProps, RangeValue } from "./datepicker/DatePicker";
-
-export { TimePicker } from "./timepicker/TimePicker";
-export type { TimePickerProps, TimeValue } from "./timepicker/TimePicker";
-
-export { Calendar } from "./calendar/Calendar";
-export type { CalendarProps } from "./calendar/Calendar";
-
-export { CodeBlock, CodeBlockCopyButton } from "./codeblock/CodeBlock";
-export type { CodeBlockProps, CodeBlockCopyButtonProps } from "./codeblock/CodeBlock";
-
-export { OverlayScroll } from "./scroll/OverlayScroll";
-export type { OverlayScrollProps, OverlayScrollVisibility } from "./scroll/OverlayScroll";
-
-export {
-  ChatThinking,
-  ChatUserMessage,
-  ChatAgentMessage,
-  ChatError,
-  ChatToolCall,
-  ChatInput,
-  ChatWelcome,
-  ChatSpinner,
-  ChatMarkdown,
-  createChatMarkdownComponents,
-  chatMarkdownComponents,
-  chatMarkdownClass,
-  chatBodyClass,
-  MermaidBlock,
-  AgentPanel,
-  useAgentStream,
-  buildAgentHistory,
-  parseSseStream,
-  formatToolName,
-  prettyJson,
-  isCallAgentToolName,
-  parseCallAgentToolTargetId,
-  matchesToolName,
-  matchesToolHook,
-  parseBgTaskRef,
-  formatBgElapsed,
-  resolveToolUI,
-  builtinToolUis,
-  matchesToolUIName,
-  isToolRunning,
-  CallAgentToolUI,
-  WebFetchToolUI,
-  GetCurrentTimeToolUI,
-  ReadSkillToolUI,
-  RunJsToolUI,
-  BackgroundTaskToolUI,
-  BackgroundTasksBar,
-  ToolUiTrailing,
-} from "./chat";
-export type {
-  ChatThinkingProps,
-  ChatUserMessageProps,
-  ChatAgentMessageProps,
-  ChatErrorProps,
-  ChatToolCallProps,
-  ChatInputProps,
-  ChatWelcomeProps,
-  ChatMarkdownProps,
-  ChatMarkdownStreamState,
-  MermaidBlockProps,
-  AgentPanelProps,
-  AgentMessage,
-  AgentMessageRole,
-  AgentHistoryMessage,
-  AgentStreamRequest,
-  AgentPanelEndpoint,
-  AgentToolAction,
-  AgentToolHook,
-  AgentToolCallEvent,
-  AgentToolResultEvent,
-  AgentToolNameMatch,
-  ToolUIProps,
-  ChatToolMessage,
-  AgentToolUI,
-  AgentToolUIName,
-  ChatBgTask,
-  BackgroundTasksBarProps,
-} from "./chat";
-
-export { cn } from "./lib/cn";
-export { glassSurfaceClass, glassOverlayClass, meadowSurfaceClass } from "./lib/surface";
-export { CONTROL_SIZES, normalizeSize, getSizeTokens, controlHeightVar, controlRadiusVar, controlStatusClass, useControlSize } from "./lib/sizes";
-export type { ControlSize, CanonicalSize, ControlSizeTokens } from "./lib/sizes";
-export { placementToRadix } from "./lib/placement";
-export type { PopperPlacement } from "./lib/placement";
-export { NONLA_THEME_KNOBS, NONLA_THEME_KEYS, applyNonlaTheme, getDesignToken } from "./theme";
-export type { NonlaThemeKnob, NonlaThemeKnobName, NonlaThemeColorName, NonlaThemeColors, NonlaThemeConfig, NonlaTokenSnapshot } from "./theme";
-
 export { FluentIcon } from "./icon/FluentIcon";
 export {
-  ICON_PREFIX,
   DEFAULT_ICON_NAME,
   DEFAULT_TOOL_ICON,
   ensureFluentIcons,
-  getIconNames,
-  getFluentImgSrc,
-  isSvgIcon,
-  isFluentIcon,
   fluentIconName,
   fluentIconRef,
+  getFluentImgSrc,
+  getIconNames,
+  ICON_PREFIX,
+  isFluentIcon,
+  isSvgIcon,
 } from "./icon/fluent";
-
-export { DesktopStage } from "./desktop/DesktopStage";
-export { MeadowWallpaper } from "./desktop/MeadowWallpaper";
-export { MeadowDesktop } from "./desktop/MeadowDesktop";
-export { MeadowShell } from "./desktop/MeadowShell";
-export { DesktopHeader, DesktopBarButton, DesktopBarItem, DesktopBarDivider } from "./desktop/DesktopHeader";
-export { DesktopIcon } from "./desktop/DesktopIcon";
-export { DesktopWindow, WindowHeader } from "./desktop/DesktopWindow";
-
-export { Menu, MenuTrigger, MenuItem, MenuAction, MenuDivider } from "./menu/Menu";
-export type { MenuRootProps, MenuTriggerProps, MenuItemProps } from "./menu/Menu";
+export { cn } from "./lib/cn";
+export type { PopperPlacement } from "./lib/placement";
+export { placementToRadix } from "./lib/placement";
+export type { CanonicalSize, ControlSize, ControlSizeTokens } from "./lib/sizes";
+export { CONTROL_SIZES, controlHeightVar, controlRadiusVar, controlStatusClass, getSizeTokens, normalizeSize, useControlSize } from "./lib/sizes";
+export { glassOverlayClass, glassSurfaceClass, meadowSurfaceClass } from "./lib/surface";
+export type { MenuItemProps, MenuRootProps, MenuTriggerProps } from "./menu/Menu";
+export { Menu, MenuAction, MenuDivider, MenuItem, MenuTrigger } from "./menu/Menu";
+export type { MessageConfig, MessageType } from "./message/message";
+export { message } from "./message/message";
+export type { ModalConfirmProps, ModalProps } from "./modal/Modal";
+export { Modal } from "./modal/Modal";
+export type { PaginationAlign, PaginationItemType, PaginationProps, PaginationSemanticSlot, PaginationSizeChangerProps } from "./pagination/Pagination";
+export { Pagination } from "./pagination/Pagination";
+export type { PopconfirmProps } from "./popconfirm/Popconfirm";
+export { Popconfirm } from "./popconfirm/Popconfirm";
+export type { PopoverProps } from "./popover/Popover";
+export { Popover } from "./popover/Popover";
+export type { OverlayScrollProps, OverlayScrollVisibility } from "./scroll/OverlayScroll";
+export { OverlayScroll } from "./scroll/OverlayScroll";
+export type { SegmentedOption, SegmentedProps } from "./segmented/Segmented";
+export { Segmented } from "./segmented/Segmented";
+export type { SelectOptionConfig, SelectProps, SelectValue } from "./select/Select";
+export { Select, SelectOption } from "./select/Select";
+export type { ShimmerProps } from "./shimmer/Shimmer";
+export { Shimmer } from "./shimmer/Shimmer";
+export type { SkeletonProps } from "./skeleton/Skeleton";
+export { Skeleton } from "./skeleton/Skeleton";
+export type { SpinProps, SpinVariant } from "./spin/Spin";
+export { Spin } from "./spin/Spin";
+export type { SplitterOrientation, SplitterPanelProps, SplitterProps, SplitterSemanticSlot, SplitterSize } from "./splitter/Splitter";
+export { Splitter, SplitterPanel } from "./splitter/Splitter";
+export type { SwitchProps, SwitchVariant } from "./switch/Switch";
+export { Switch } from "./switch/Switch";
+export type {
+  ColumnsType,
+  ColumnType,
+  SortOrder,
+  TablePaginationConfig,
+  TableProps,
+  TableRowSelection,
+} from "./table/Table";
+export { Table } from "./table/Table";
+export type { TagProps, TagVariant } from "./tag/Tag";
+export { Tag } from "./tag/Tag";
+export type { NonlaThemeColorName, NonlaThemeColors, NonlaThemeConfig, NonlaThemeKnob, NonlaThemeKnobName, NonlaTokenSnapshot } from "./theme";
+export { applyNonlaTheme, getDesignToken, NONLA_THEME_KEYS, NONLA_THEME_KNOBS } from "./theme";
+export type { TimePickerProps, TimeValue } from "./timepicker/TimePicker";
+export { TimePicker } from "./timepicker/TimePicker";
+export type { TooltipPlacement, TooltipProps } from "./tooltip/Tooltip";
+export { Tooltip } from "./tooltip/Tooltip";
